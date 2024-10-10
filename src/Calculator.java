447f72aa2d9298ad236d587fd2cbab26e42ac1ae
umarraza89 <umermuh908@gmail.com>
2024-10-09 21:44:28 -0700

    added add func and mult func validation

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
 
 public class Calculator {
 
 	public Calculator() {
+		
+		
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Enter number 1");
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		System.out.println("Enter number 2");
+		int b=s.nextInt();
+		if()
+		System.out.println("number 1 is"+a);
+		System.out.println("number 2 is"+b);
 
 	}
 
