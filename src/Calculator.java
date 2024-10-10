7820526a01c44ed80db4f73b76add158179a523e
Ahmad Raza <f223740@cfd.nu.edu.pk>
2024-10-10 10:01:43 +0500

    resolve Conflict

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,22 +2,44 @@ import java.util.Scanner;
 
 public class Calculator {
 
-	public Calculator() {
-		
-		
-		// TODO Auto-generated constructor stub
-	}
+    public Calculator() 
+    {
+        // Constructor code if needed
+    }
 
-	public static void main(String[] args) {
-		System.out.println("Enter number 1");
-		Scanner s=new Scanner(System.in);
-		int a=s.nextInt();
-		System.out.println("Enter number 2");
-		int b=s.nextInt();
-		if()
-		System.out.println("number 1 is"+a);
-		System.out.println("number 2 is"+b);
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
 
-	}
+        // Input number 1
+        System.out.println("Enter number 1");
+        int a = s.nextInt();
 
+        // Input number 2
+        System.out.println("Enter number 2");
+        int b = s.nextInt();
+
+        // Perform calculations if both numbers are greater than 0
+        if (a > 0 && b > 0) {
+            System.out.println("Addition: " + addition(a, b));
+            System.out.println("Multiplication: " + multiplication(a, b));
+        } 
+        else
+        {
+            System.out.println("Both numbers must be greater than zero.");
+        }
+
+        // Close the Scanner to avoid resource leaks
+        s.close();
+    }
+
+    static int addition(int num1, int num2) 
+    {
+        return num1 + num2;
+    }
+
+    static int multiplication(int num1, int num2) 
+    {
+        return num1 * num2;
+    }
 }
